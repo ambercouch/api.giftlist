efaa440fe68d266df14db8a1b19ce92706f73271
richiearnold <richard@ambercouch.co.uk>
2016-12-29 18:58:02 +0000

    Added token to users table

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -18,6 +18,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'api_token' => str_random(60),
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
